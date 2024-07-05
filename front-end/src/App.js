c0ba8c2e4fa32624c30d25ad230b81bf451ea09c
Thrishalmadasu <thrishalmadasu@gmail.com>
2024-07-05 22:32:13 +0530

    Added Product Card and Product Listing Components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
+import ProductListing from './components/ProductListing/ProductListing';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Search/>
+      <ProductListing/>
+
     </div>
   );
 }
