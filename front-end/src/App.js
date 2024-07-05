6bc05a71b9774a806461d68f4f55561298164849
Rohith Thirunagari <trohith.2006@gmail.com>
2024-07-05 22:33:30 +0530

    Footer created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <Search/>
+      <Footer/>
     </div>
   );
 }
