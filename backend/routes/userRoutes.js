d0477af81d0ce72b181b47fdeeb2720000ebe312
Thrishalmadasu <thrishalmadasu@gmail.com>
2024-08-19 22:30:59 +0530

    bcrypt -> bcryptjs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const authMiddleware = require("../middlewares/authMiddleware");
 const jwt = require('jsonwebtoken');
 
