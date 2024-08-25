2f692987685a9820635e34b8fa954d4627a397ae
Rohith Thirunagari <trohith.2006@gmail.com>
2024-08-25 16:07:09 +0530

    updated ProductCard css

--- frontend/src/components/ProductCard/ProductCard.js
+++ frontend/src/components/ProductCard/ProductCard.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ProductCard.css';
 
-const ProductCard = ({ id, image, name, price }) => {
+const ProductCard = ({ id, image, name , price}) => {
   return (
-    <Link to={`/product/${id}`} className="product-card-link">
-      <article className="product-card">
-        <figure className="product-image-container">
-          <img src={image} alt={name} className="product-image" />
-        </figure>
-        <section className="product-info">
+    <div className="product-card">
+      <Link to={`/product/${id}`} className="product-card-link">
+        <img src={image} alt={name} className="product-image" />
+        <div className="product-info">
           <h3 className="product-name">{name}</h3>
           <p className="product-price">₹{price}</p>
-        </section>
-      </article>
-    </Link>
+        </div>
+      </Link>
+    </div>
   );
 };
 
